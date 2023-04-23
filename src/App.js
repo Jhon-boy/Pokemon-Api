e66c7c06719fbc278ae343ec282447b5930eb904
Jhon <jhoncuvi12@gmail.com>
2023-04-22 22:10:40 -0500

    Actualizacion Finall

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import  AppRouter  from './AppRouter';
+import React from 'react';
 import { PokemonProvider } from './context/PokemonProvider';
 
 function App() {
   return (
-    <PokemonProvider>
+    <React.StrictMode>
+       <PokemonProvider>
       <AppRouter />
     </PokemonProvider>
+    </React.StrictMode>
+   
   );
 }
 
